6fe2f761d96c25c1e6434a75f8571278439f2a07
33haji <one_for_all_0828@yahoo.co.jp>
2017-02-05 13:20:21 +0000

    tableページを作成

--- app/controllers/manage_controller.rb
+++ app/controllers/manage_controller.rb
@@ -5,6 +5,11 @@ class ManageController < ApplicationController
   
   def column_list
     @table_name = params[:table]
+    # table_nameを単数形・キャメル・クラスオブジェクトに変換する
+    capitalized_table_name = @table_name.singularize.camelcase.constantize
+    @column_names = capitalized_table_name.columns.map{ |column| column.name}
+    @column_types = capitalized_table_name.columns.map{ |column| column.type}
+    @columns = capitalized_table_name.all.order(id: :desc)
   end
   
 end
